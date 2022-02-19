a924f300b74ccf08407d4b3d4f7579d4ef563dd5
Elena Safina <elena.shxx@gmail.com>
2022-02-20 00:45:43 +0200

    Поправить жс (попап, лишние переменные, сабмит на форму)

--- script/index.js
+++ script/index.js
@@ -2,8 +2,8 @@
 let popupEditProfile = document.querySelector('.popup'); // Попап редактировать профиль
 let buttonCloseEditProfile = document.querySelector('.popup__close-form'); // Закрыть попап
 let buttonEditProfile = document.querySelector('.profile__button-edit'); // Редактировать профиль
-let buttonSubmitEditProfile = document.querySelector('.popup__form-submit'); // Сохранить попап ред профиля
 
+let formSubmit = document.querySelector('.popup__form'); // Форма
 let nameProfileInput = document.querySelector('.popup__input_form_name'); // Данные ИМЯ в инпуте
 let aboutProfileInput = document.querySelector('.popup__input_form_about'); // Данные О СЕБЕ в инпуте
 let profileInfoName = document.querySelector('.profile__info-name'); // Данные ИМЯ в самом профиле
@@ -22,19 +22,15 @@ function openPopup() {
     aboutProfileInput.value = profileInfoAbout.textContent; // В инпут берутся данные из профиля
 } // Прописываю функцию, открывающую попап кликом и подгружающую в инпуты данные из профиля
 
-function savePopup() {
+function savePopup(evt) {
+    evt.preventDefault();  // Отменяет стандартную отправку формы
     profileInfoName.textContent = nameProfileInput.value; // Из инпута данные летят в профиль
     profileInfoAbout.textContent = aboutProfileInput.value; // Из инпута данные летят в профиль
     closePopup(); // Автоматически закрыть попап
 } // Прописываю функцию, передающую из инпутов в данные профиля
 
-
-// Вызываю функции
-closePopup(); // Вызываю функцию, закрывающую попап при чтении страницы
-
-
 // Прописываю события
 buttonEditProfile.addEventListener("click", openPopup); //Вызываю функцию, открывающую попап кликом на карандашик
 buttonCloseEditProfile.addEventListener("click", closePopup); // Прописываю функцию, закрывающую попап кликом на крестик
-buttonSubmitEditProfile.addEventListener("click", savePopup); // Прописываю функцию, сохраняющую значения кликом на сохранить
+formSubmit.addEventListener("submit", savePopup);// Прописываю функцию, сохраняющую значения формы
 // Прописываю функцию, открывающую добавить пост (пока что не делаем)
