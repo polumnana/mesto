36bb50a5da5c33ea43430b3265876c3fde96208d
Elena Safina <elena.shxx@gmail.com>
2022-05-05 06:33:48 +0300

    Итоговая структура проекта

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // подключили плагин
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: { main: './script/index.js' },
+    entry: { main: './src/script/index.js' },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index.js',
@@ -24,17 +24,12 @@ module.exports = {
                 use: 'babel-loader',
                 exclude: '/node_modules/'
             },
-            // добавили правило для обработки файлов
             {
-                // регулярное выражение, которое ищет все файлы с такими расширениями
                 test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
                 type: 'asset/resource'
             },
             {
-                // применять это правило только к CSS-файлам
                 test: /\.css$/,
-                // при обработке этих файлов нужно использовать
-                // MiniCssExtractPlugin.loader и css-loader
                 use: [
                     MiniCssExtractPlugin.loader,
                     {
@@ -48,9 +43,9 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './index.html' // путь к файлу index.html
+            template: './src/index.html'
         }),
         new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin() // подключение плагина для объединения файлов
+        new MiniCssExtractPlugin(),
     ]
-};
+}
