1a73ed11e0c8ff3b837d9002eb7bdd606e4b8a72
polumnana <elena.shxx@gmail.com>
2022-05-05 07:46:37 +0300

    Section ok

--- src/components/Section.js
+++ src/components/Section.js
@@ -5,5 +5,17 @@ class Section {
 
         this._container = document.querySelector(containerSelector);
     }
-    addItem() { }// Этот метод принимает DOM-элемент и добавляет его в контейнер
+
+    addItem(element) {
+        this._container.prepend(element);
+    }// принимает DOM-элемент и добавляет его в контейнер
+
+    renderItems() {
+        this.clear();
+
+        this._renderedItems.forEach(item => {
+            this._renderer(item);
+        });
+    }
+
 }
