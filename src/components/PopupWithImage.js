232ceb87aaf8d37435c40aa092f0de97c2f333a6
polumnana <elena.shxx@gmail.com>
2022-05-05 11:24:07 +0300

    PopupWithImage

---
+++ src/components/PopupWithImage.js
@@ -0,0 +1,17 @@
+import Popup from './Popup.js';
+
+export default class PopupWithImage extends Popup {
+    constructor(popup) {
+        super(popup);
+        this._popup = popup;
+        this._previewImg = this.popup.querySelector('.popup__img'); // Крупное фото попап 
+        this._previewText = this.popup.querySelector('.popup__text'); // Описание фото попап 
+    }
+    open(name, link) {
+        this._previewImg.setAttribute('src', link); // Передали карточке SRC 
+        this._previewImg.setAttribute('alt', name); // Передали карточке ALT 
+        this._previewText.textContent = name;  // Передали карточке заголовок
+
+        super.open();
+    }
+}
